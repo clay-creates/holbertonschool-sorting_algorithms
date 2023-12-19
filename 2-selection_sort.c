8051523ba40395a239235c2192cfd56c26527732
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-12-19 18:07:40 +0000

    move print

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,26 +1,34 @@
 #include "sort.h"
 
-/***/
+/**
+ * selection_sort - sort array of ints in ascending order
+ * @array: array to sort
+ * @size: size of array
+ */
 
 void selection_sort(int *array, size_t size)
 {
-    int temp, min_idx;
-    size_t i, j;
+	int temp, min_idx;
+	size_t i, j;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        min_idx = i;
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[min_idx])
-            {
-                min_idx = j;
-            }
-        }
-        temp = array[min_idx];
-        array[min_idx] = array[i];
-        array[i] = temp;
+	if (array == NULL)
+	{
+		return;
+	}
 
-        print_array(array, size);
-    }
+	for (i = 0; i < size - 1; i++)
+	{
+		min_idx = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_idx])
+			{
+				min_idx = j;
+			}
+		}
+		temp = array[min_idx];
+		array[min_idx] = array[i];
+		array[i] = temp;
+	}
+	print_array(array, size);
 }
