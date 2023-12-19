b958e8727c8d2f327c2c3fb72284ceed231f732b
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-12-19 21:36:46 +0000

    insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,41 +9,43 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *sorted, *current, *next, *current_sorted;
 
+	if (list == NULL || *list == NULL)
+	{
+		return;
+	}
+
 	sorted = NULL;
 	current = *list;
 
 	while (current != NULL)
 	{
 		next = current->next;
-		current_sorted = sorted;
-
 		current->prev = current->next = NULL;
 
 		if (sorted == NULL || sorted->n >= current->n)
 		{
 			current->next = sorted;
 			if (sorted != NULL)
-			{
 				sorted->prev = current;
-				sorted = current;
-			}
-			else
-			{
-				while (current_sorted->next != NULL && current_sorted->next->n < current->n)
-				{
-					current_sorted = current_sorted->next;
-					current->next = current_sorted->next;
-				}
-				if (current_sorted->next != NULL)
-				{
-					current_sorted->next->prev = current;
-					current_sorted->next = current;
-					current->prev = current_sorted;
-				}
-			}
-			print_list(sorted);
-			current = next;
+			sorted = current;
 		}
-		*list = sorted;
+		else
+		{
+			current_sorted = sorted;
+
+			while (current_sorted->next != NULL && current_sorted->next->n < current->n)
+				current_sorted = current_sorted->next;
+
+			current->next = current_sorted->next;
+			if (current_sorted->next != NULL)
+				current_sorted->next->prev = current;
+			current_sorted->next = current;
+			current->prev = current_sorted;
+		}
+
+		print_list(sorted);
+		current = next;
 	}
+
+	*list = sorted;
 }
