ee4224779bcfe1026eb7bb019a46b8f0bd69568d
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-12-19 17:52:23 +0000

    selection plus swap

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -4,4 +4,22 @@
 
 void selection_sort(int *array, size_t size)
 {
+    int a, b, i, j, temp, min_idx;
+
+    for (i = 0; i < size - 1; i++)
+    {
+        min_idx = i;
+        for (j = i + 1; j < size; j++)
+        {
+            if (array[j] < array[min_idx])
+            {
+                min_idx = j;
+            }
+        }
+        temp = array[min_idx];
+        array[min_idx] = array[i];
+        array[i] = temp;
+
+        print_array(array, size);
+    }
 }
