9009a5390518a45672e038d35f8639bc04f672b9
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-12-19 21:08:28 +0000

    rewrite and print

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,49 +1,6 @@
 #include "sort.h"
 
 /**
- * swap - swaps two values
- * @a: first value
- * @b: second value
- */
-
-void swap(int *a, int *b)
-{
-	int temp;
-
-	temp = *a;
-	*a = *b;
-	*b = temp;
-}
-
-/**
- * partition - partitions an array using Lamuto scheme
- * @array: array to partition
- * @low: value / space before array starts
- * @high: final value in array
- *
- * Return: returns index of pivot after partitioning
- */
-
-int partition(int *array, int low, int high)
-{
-	int i, j, pivot;
-
-	pivot = array[high];
-	i = (low - 1);
-
-	for (j = low; j <= high - 1; j++)
-	{
-		if (array[j] <= pivot)
-		{
-			i++;
-			swap(&array[i], &array[j]);
-		}
-	}
-	swap(&array[i + 1], &array[high]);
-	return (i + 1);
-}
-
-/**
  * quick_sort - sorts an array of integers in ascending order
  * @array: array to sort
  * @size: size of array
@@ -51,18 +8,38 @@ int partition(int *array, int low, int high)
 
 void quick_sort(int *array, size_t size)
 {
-	int low, high, part;
+	int i, j, pivot;
 
-	low = 0;
-	high = size - 1;
+	pivot = array[size / 2];
+	i = 0;
+	j = size - 1;
 
-	if (low < high)
+	if (size < 2)
 	{
-		part = partition(array, low, high);
-
-		print_array(array, size);
-
-		quick_sort(array, part);
-		quick_sort(array + part + 1, size - part - 1);
+		return;
 	}
+
+	while (i <= j)
+	{
+		while (array[i] < pivot)
+		{
+			i++;
+		}
+		while (array[j] > pivot)
+		{
+			j--;
+		}
+		if (i <= j)
+		{
+			int temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
+			i++;
+			j--;
+		}
+		print_array(array, size);
+	}
+
+	quick_sort(array, j + 1);
+	quick_sort(array + i, size - i);
 }
